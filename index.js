584ef436bb7e8c3a139ba3e0917b9d756ec9b1be
yurifrl <yurifrl@hotmail.com>
2017-01-17 15:10:50 -0200

    test

--- index.js
+++ index.js
@@ -2,35 +2,36 @@ const { firebase, $http, $scope } = require('./stubs.js')
 
 // firebase.auth().signInWithPopup("").then(console.log).catch(console.log)
 
-const provider = {  }
+const provider = { }
+const getMoreData = http => token => {
+  const url = 'https://graph.facebook.com/v2.5/me?access_token=' + token + '&fields=id,name,first_name,last_name,email'
+  $http.get(url).success(function(jsonService){
+    $scope.user.firstName= jsonService.first_name;
+    $scope.user.lastName = jsonService.last_name
+    $scope.user.email = jsonService.email;
+    $scope.user.password=null;
+    $scope.user.tokenFB=token;
+    // ?
+    // $scope.user=true;
+    console.log($scope.user);
+  });
+}
+const getMoreDataWithHttp($http)
 
 $scope.FBLogin = function () {
   firebase.auth().signInWithPopup(provider).then(function(result) {
-
     var token = result.credential.accessToken;
     var user = result.user;
 
-    const url = 'https://graph.facebook.com/v2.5/me?access_token=' + token + '&fields=id,name,first_name,last_name,email'
-    $http.get(url).success(function(jsonService){
-        $scope.user.firstName= jsonService.first_name;
-        $scope.user.lastName = jsonService.last_name
-        $scope.user.email = jsonService.email;
-        $scope.user.password=null;
-        $scope.user.tokenFB=token;
-        // ?
-        // $scope.user=true;
-        console.log($scope.user);
+    getMoreDataWithHttp(token)
 
-        if($scope.authenticate()){
-          console.log("Logged in with Facebook! :)")
-
-        } else {
-          $scope.checkingUser=false;
-          $scope.Register();
-          $scope.Authenticate();
-        };
-      });
-
+    if($scope.authenticate()){
+      console.log("Logged in with Facebook! :)")
+    } else {
+      $scope.checkingUser=false;
+      $scope.Register();
+      $scope.Authenticate();
+    };
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
